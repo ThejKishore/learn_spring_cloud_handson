db38d7bd56e1e6612b55e1d838531a0c442a1c0c
ThejKishore <kishores1984@gmail.com>
2019-09-23 20:15:28 -0500

    added the prometheus metrics

--- adminbootserver/src/main/java/com/kish/learning/adminbootserver/AdminbootserverApplication.java
+++ adminbootserver/src/main/java/com/kish/learning/adminbootserver/AdminbootserverApplication.java
@@ -1,12 +1,14 @@
 package com.kish.learning.adminbootserver;
 
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import de.codecentric.boot.admin.server.domain.entities.Instance;
 import de.codecentric.boot.admin.server.web.client.HttpHeadersProvider;
 import de.codecentric.boot.admin.server.web.client.InstanceExchangeFilterFunction;
 import de.codecentric.boot.admin.server.web.client.InstanceWebClient;
 import io.netty.handler.ssl.SslContext;
 import io.netty.handler.ssl.SslContextBuilder;
 import io.netty.handler.ssl.util.InsecureTrustManagerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -14,9 +16,14 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.client.reactive.ClientHttpConnector;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
+import org.springframework.web.reactive.function.client.ClientRequest;
+import org.springframework.web.reactive.function.client.ClientResponse;
+import org.springframework.web.reactive.function.client.ExchangeFunction;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 import reactor.netty.http.client.HttpClient;
 
+import java.net.URI;
 import java.time.Duration;
 import java.util.Collections;
 import java.util.List;
@@ -25,6 +32,7 @@ import java.util.List;
 @SpringBootApplication
 @EnableAdminServer
 @EnableDiscoveryClient
+@Slf4j
 public class AdminbootserverApplication {
 
     private WebClient constructWebClient(){
@@ -63,6 +71,23 @@ public class AdminbootserverApplication {
                 .filters(filters -> filters.addAll(additionalFilters))
                 .webClient(constructWebClient())
                 .build();
+    }
+
+    @Bean
+    public InstanceExchangeFilterFunction auditLog() {
+        return (instance, request, next) -> modifyingTheRequest(instance, request, next);
+    }
+
+    private  Mono<ClientResponse> modifyingTheRequest(Instance instance, ClientRequest request, ExchangeFunction next){
+        log.info(" path --> {} , raw path  -> {}   complete url --> {} ",request.url().getPath() , request.url().getRawPath(),request.url().toString());
+        log.info("{} for {} on {}", request.method(), instance.getId(), request.url());
+
+        String completeUrl = request.url().toString();
+        URI uri = URI.create(completeUrl.replace("/actuator","/admin"));
+
+        ClientRequest request1 = ClientRequest.from(request).url(uri).build();
+
+        return next.exchange(request);
     }
 
     public static void main(String[] args) {
--- dicoveryclientdemo/src/main/java/com/kish/learning/dicoveryclientdemo/DicoveryclientdemoApplication.java
+++ dicoveryclientdemo/src/main/java/com/kish/learning/dicoveryclientdemo/DicoveryclientdemoApplication.java
@@ -1,15 +1,28 @@
 package com.kish.learning.dicoveryclientdemo;
 
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableEurekaClient
 public class DicoveryclientdemoApplication {
 
+    private String instanceId="something";
+
     public static void main(String[] args) {
         SpringApplication.run(DicoveryclientdemoApplication.class, args);
     }
 
+    @Bean
+    MeterRegistryCustomizer<MeterRegistry> metricsCommonTags() {
+        return registry -> {
+            registry.config().commonTags("region", "us-east-1");
+            registry.config().commonTags("instanceId", instanceId);
+        };
+    }
 }
